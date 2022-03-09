08c3a5b713abc686c73d6bc609be70308c25a85a
Ashton Li <ashton.li@sjsu.edu>
2022-03-08 17:55:24 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,21 @@
+//sports cars
+let cars = [{
+  name: "Scion FR-S",
+}, {
+  name: "Mazda RX-7"
+}, {
+  name: "Toyota Supra"
+}, {
+  name: "Nissan GT-R"
+}, {
+  name: "Honda NSX"
+}, {
+  name: "Mitsubishi Lancer Evolution"
+}, {
+  name: "Subaru Impreza WRX STI"
+}
+}];
+
 function setup() {
   createCanvas(400, 400);
 }
