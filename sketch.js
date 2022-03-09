03dbe92daed4af386f3565583f085ec20b4523a5
Ashton Li <ashton.li@sjsu.edu>
2022-03-08 18:01:34 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,22 +4,40 @@ let cars = [{
 }, {
   name: "Mazda RX-7"
 }, {
+  name: "Mazda Miata"
+}, {
   name: "Toyota Supra"
+}, {
+  name: "Toyota MR-2"
 }, {
   name: "Nissan GT-R"
 }, {
+  name: "Nissan 300ZX"
+}, {
   name: "Honda NSX"
+}, {
+  name: "Honda S2000"
 }, {
   name: "Mitsubishi Lancer Evolution"
 }, {
+  name: "Mitsubishi Eclipse"
+}, {
   name: "Subaru Impreza WRX STI"
-}
+}, {
+  name: "Subaru BRZ"
 }];
+
+let randomIndex;
 
 function setup() {
   createCanvas(400, 400);
+  background(220);
+
+  randomIndex = int(random(cars.length));
+  console.log(cars[randomIndex].name);
+  text(cars[randomIndex].name, 200, 200);
 }
 
 function draw() {
-  background(220);
+
 }
