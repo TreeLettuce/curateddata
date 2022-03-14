5ac0d80e578f933355a95e6a9240dd77e311acde
Ashton Li <ashton.li@sjsu.edu>
2022-03-14 11:41:37 -0700

    added car images

--- sketch.js
+++ sketch.js
@@ -1,50 +1,35 @@
-//sports cars
-let cars = [{
-  name: "Scion FR-S",
-}, {
-  name: "Mazda RX-7"
-}, {
-  name: "Mazda Miata"
-}, {
-  name: "Toyota Supra"
-}, {
-  name: "Toyota MR-2"
-}, {
-  name: "Nissan GT-R"
-}, {
-  name: "Nissan 300ZX"
-}, {
-  name: "Honda NSX"
-}, {
-  name: "Honda S2000"
-}, {
-  name: "Mitsubishi Lancer Evolution"
-}, {
-  name: "Mitsubishi Eclipse"
-}, {
-  name: "Subaru Impreza WRX STI"
-}, {
-  name: "Subaru BRZ"
-}];
+let cars = [];
+let numCars = 8;
 
-let randomIndex;
+function preload() {
+  let car0 = loadImage('car0.png');
+  let car1 = loadImage('car1.png');
+  let car2 = loadImage('car2.png');
+  let car3 = loadImage('car3.png');
+  let car4 = loadImage('car4.png');
+  let car5 = loadImage('car5.png');
+  let car6 = loadImage('car6.png');
+  let car7 = loadImage('car7.png');
+  cars = [car0,car1,car2,car3,car4,car5,car6,car7];
+  }
 
-function setup() {
-  createCanvas(400, 400);
-  background(220);
+//let randomIndex;
 
-}
+  function setup() {
+    createCanvas(1000, 1000);
+    noLoop();
+  }
 
-function draw() {
+  function draw() {
+    background(220);
+    imageMode(CENTER);
+    let randomCar = random(cars);
+    image(randomCar);
+  }
 
-}
-
-function mousePressed() {
-  background(220);
-  
-  randomIndex = int(random(cars.length));
-  console.log(cars[randomIndex].name);
-  text(cars[randomIndex].name, 200, 200);
-  cars.splice(randomIndex, 1);
-
-}
+//function mousePressed() {
+    //randomIndex = int(random(cars.length));
+    //console.log(cars[randomIndex].name);
+    //text(cars[randomIndex].name, 200, 200);
+    //cars.splice(randomIndex, 1);
+//}
